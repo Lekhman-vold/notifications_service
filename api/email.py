134c318ffe559cd97d805ca25ed62dca58ba86ed
Lekhman-vold <vladgrojsman6@gmail.com>
2023-02-24 18:16:10 +0200

    added packages, update identation

--- api/email.py
+++ api/email.py
@@ -15,33 +15,33 @@ router = APIRouter()
 
 @router.post("/send-email/", status_code=status.HTTP_200_OK)
 async def sender(schema: SendEmailModel):
-	subject = schema.subject
-	from_email = schema.from_email
-	to_email = schema.to_email
-	html = schema.html
+    subject = schema.subject
+    from_email = schema.from_email
+    to_email = schema.to_email
+    html = schema.html
 
-	try:
-		msg = MIMEText(html, "html")
-		msg['Subject'] = subject
-		msg['From'] = from_email
-		msg['To'] = to_email
+    try:
+        msg = MIMEText(html, "html")
+        msg['Subject'] = subject
+        msg['From'] = from_email
+        msg['To'] = to_email
 
-		# Connect to the email server
-		server = SMTP_SSL(
-			settings.smtp.server,
-			settings.smtp.port
-		)
-		server.login(
-			settings.smtp.username,
-			settings.smtp.password
-		)
+        # Connect to the email server
+        server = SMTP_SSL(
+            settings.smtp.server,
+            settings.smtp.port
+        )
+        server.login(
+            settings.smtp.username,
+            settings.smtp.password
+        )
 
-		# Send the email
-		server.send_message(msg)
-		server.quit()
-		return {
-			"message": "email sent successfully {0}".format(datetime.now())
-		}
+        # Send the email
+        server.send_message(msg)
+        server.quit()
+        return {
+            "message": "email sent successfully {0}".format(datetime.now())
+        }
 
-	except Exception as e:
-		raise HTTPException(status_code=500, detail=e)
+    except Exception as e:
+        raise HTTPException(status_code=500, detail=e)
